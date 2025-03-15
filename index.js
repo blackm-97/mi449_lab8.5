dd6718a8e7e533670e5d1c1f8ec7a55361b99a57
Connor Blackmer <blackm97@msu.edu>
2025-03-15 15:03:10 -0400

    Supabase Link

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js"
 const supabaseUrl = 'https://tmqexmpecrveukczziib.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcWV4bXBlY3J2ZXVrY3p6aWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MjUyODIsImV4cCI6MjA1NzQwMTI4Mn0.jNeYIZsVxiHIBow6cPuOkAaB7vFGr3MeqTgDcQNNI_4'
 const supabase = createClient(supabaseUrl, supabaseKey)
