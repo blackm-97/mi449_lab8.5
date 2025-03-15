5512743cd9ce1670a8439daac7869899a65065f2
Connor Blackmer <blackm97@msu.edu>
2025-03-15 15:33:11 -0400

    Fixed datastore

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const supabaseUrl = 'https://tmqexmpecrveukczziib.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcWV4bXBlY3J2ZXVrY3p6aWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MjUyODIsImV4cCI6MjA1NzQwMTI4Mn0.jNeYIZsVxiHIBow6cPuOkAaB7vFGr3MeqTgDcQNNI_4'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getBooks() {
-    let { data: Library, error } = await supabase
-    .from('Library')
-    .select('*')
+async function getLibrary() {
 
-    let bookList = document.getElementById('bookTable');
+let { data: Library, error } = await supabase
+  .from('Library')
+  .select('*')
 
-    for(let book in Library){
-        bookList += '<tr><td>${book.title}</td></tr>'
-    }
+  let bookList = document.getElementById('books');
+  
+  for (let book of Library) {
+     bookList.innerHTML += `<tr><td>${book.title}</td>
+     </tr>
+     `;
+   }
 }
 
-getBooks();
+getLibrary();
